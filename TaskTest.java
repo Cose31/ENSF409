2a66bdeef4a18ad69c86e7d189e1f0097c2c77f4
Cose31 <115685477+Cose31@users.noreply.github.com>
2023-03-23 16:09:11 -0600

    Update TaskTest.java

--- TaskTest.java
+++ TaskTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class TaskTest 
 {
     //testing values for Task
-    Task task = new Task(1, "Task 1", 60, 120);
+    Task task = new Task(1, "Task 1", 40, 3);
     
     //Test of Task Constructor
     @Test 
@@ -41,64 +41,73 @@ public class TaskTest
     @Test
     public void testGetDuration() 
     {
-        assertEquals("Constructor or getter gave wrong duration", 60, task.getDuration());
+        assertEquals("Constructor or getter gave wrong duration", 40, task.getDuration());
     }
     
     //Testing of SetDuration
     @Test
     public void testSetDuration() 
     {
-        task.setDuration(90);
-        assertEquals("Setter did not update duration properly, or getter for duration is not working properly with update", 90, task.getDuration());
+        task.setDuration(30);
+        assertEquals("Setter did not update duration properly, or getter for duration is not working properly with update", 30, task.getDuration());
     }
     
     //Testing of GetTimeWindow
     @Test
     public void testGetTimeWindow() 
     {
-        assertEquals("Constructor or getter gave wrong TimeWindow", 120, task.getTimeWindow());
+        assertEquals("Constructor or getter gave wrong TimeWindow", 3, task.getTimeWindow());
     }
     
     //Testing of setTimeWindow
     @Test
     public void testSetTimeWindow() 
     {
-        task.setTimeWindow(180);
-        assertEquals("Setter did not update TimeWindow correctly, or getter for timewindow is not working properly with update", 180, task.getTimeWindow());
+        task.setTimeWindow(2);
+        assertEquals("Setter did not update TimeWindow correctly, or getter for timewindow is not working properly with update", 2, task.getTimeWindow());
     }
     
     //TaskList Testing begins here
     
-    TaskList taskList = new TaskList();
-    taskList.addTask(1, "Task 1", 40, 3);
-    
-    //Test addTask
+    //Test Constructor for TaskList
     @Test
-    public void testAddTask() {
-        ArrayList<Task> tasks = taskList.getTaskList();
-        assertEquals(1, tasks.size());
-        assertEquals(1, tasks.get(0).getTaskID());
-        assertEquals("Task 1", tasks.get(0).getDescription());
-        assertEquals(60, tasks.get(0).getDuration());
-        assertEquals(120, tasks.get(0).getTimeWindow());
-    }
-    
-    //Test clearList
-    @Test
-    public void testClearList() {
-        taskList.clearList();
-        ArrayList<Task> tasks = taskList.getTaskList();
-        assertTrue(tasks.isEmpty());
+    public void testTaskListConstructor()
+    {
+        TaskList taskList = new TaskList();
+        assertNotNull("Tasklist object not created.", taskList)
     }
     
     //Test getTaskList
     @Test
-    public void testGetTaskList() {
+    public void testGetTaskList() 
+    {
+        TaskList taskList = new TaskList();
         ArrayList<Task> tasks = taskList.getTaskList();
-        assertEquals(1, tasks.size());
-        assertEquals(1, tasks.get(0).getTaskID());
-        assertEquals("Task 1", tasks.get(0).getDescription());
-        assertEquals(60, tasks.get(0).getDuration());
-        assertEquals(120, tasks.get(0).getTimeWindow());
+        assertNotNull("TaskList method getTaskList returned null", tasks);
+        assertTrue("TaskList did not return an ArrayList of Tasks (tasks isn't of type Task)", tasks instanceof ArrayList<Task>);
+    }
+    
+    //Test addTask
+    @Test
+    public void testAddTask() 
+    {
+        TaskList taskList = new TaskList();
+        taskList.addTask(1, "Task 1", 40, 3);
+        Task tasks = taskList.getTaskList();
+        assertEquals("AddTask did not add the correct taskID.", 1, tasks.get(0).getTaskID());
+        assertEquals("AddTask did not add the correct description.", "Task 1", tasks.get(0).getDescription());
+        assertEquals("AddTask did not add the correct duration.", 40, tasks.get(0).getDuration());
+        assertEquals("AddTask did not add the correct TimeWindow.", 3, tasks.get(0).getTimeWindow());
+    }
+    
+    //Test clearList
+    @Test
+    public void testClearList() 
+    {
+        TaskList taskList = new TaskList();
+        taskList.addTask(1, "Task 1", 40, 3);
+        taskList.clearList();
+        ArrayList<Task> tasks = taskList.getTaskList();
+        assertTrue("Task List is not empty, clearList did not clear properly.", tasks.isEmpty());
     }
 }
